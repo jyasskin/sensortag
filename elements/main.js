02b99c3f44626ec568dd0372148fabd2251154eb
Jeffrey Yasskin <jyasskin@gmail.com>
2014-11-18 18:03:44 -0800

    Use the polyfill's requestDevice() function instead of chrome.bluetooth.

--- elements/main.js
+++ elements/main.js
@@ -1,8 +1,6 @@
 (function() {
 'use strict';
 
-new SensortagTemperature();
-
 // GATT Device Information Service UUIDs
 var DEVICE_INFO_SERVICE_UUID      = '0000180a-0000-1000-8000-00805f9b34fb';
 var MANUFACTURER_NAME_STRING_UUID = '00002a29-0000-1000-8000-00805f9b34fb';
@@ -21,23 +19,13 @@ var supportedServices = new Set([
 
 Polymer('sensortag-main', {
   bluetoothState: 'bluetooth-searching',
-  bluetoothEnabled: false,
 
   created: function() {
-    this.deviceMap = new Map();  // Call deviceMapChanged manually when this changes.
-    this.deviceList = [];  // List of bluetooth.Devices. Used in the UI.
+    this.selectedDevice = null;
 
     this.temperature = new SensortagTemperature();
     this.accelerometer = new SensortagAccelerometer();
     this.humidity = new SensortagHumidity();
-  },
-
-  attached: function() {
-    this.selectedDeviceAddressChanged();
-  },
-  detached: function() {
-    // If this element is removed from the DOM, stop getting Bluetooth notifications.
-    this.selectedDevice = null;
   },
 
   ready: function() {
@@ -46,34 +34,6 @@ Polymer('sensortag-main', {
     window.onerror = function(e) {
       self.error(e);
     };
-
-    // Let the UI track whether Bluetooth is enabled
-    chrome.bluetooth.onAdapterStateChanged.addListener(function(adapter) {
-      self.bluetoothEnabled = adapter.enabled;
-    });
-    chrome.bluetooth.getAdapterState(function(adapter) {
-      self.bluetoothEnabled = adapter.enabled;
-    });
-
-    // Initialize the device map.
-    chrome.bluetooth.getDevices(function (devices) {
-      if (chrome.runtime.lastError) {
-        self.error(chrome.runtime.lastError.message);
-        return;
-      }
-
-      if (devices) {
-        devices.forEach(function (device) {
-          // See if the device exposes a TI service.
-          var services = device.uuids.filter(function (service) {
-            return supportedServices.has(service);
-          });
-
-          if (services.length)
-            self.storeDevice(device.address, device);
-        });
-      }
-    });
   },
 
   error: function(message) {
@@ -83,42 +43,48 @@ Polymer('sensortag-main', {
   },
 
   scanBluetooth: function() {
-
-  },
-
-  storeDevice: function(deviceAddress, device) {
-    if (device == null) {
-      this.deviceMap.delete(deviceAddress);
-    } else {
-      this.deviceMap.set(deviceAddress, device);
-    }
-    this.deviceMapChanged();
-  },
-
-  deviceMapChanged: function() {
-    this.deviceList = [];
-    for (var device of this.deviceMap.values())
-      this.deviceList.push(device);
-    this.deviceList.sort(function(a, b) {
-      return a.name.compare(b.name);
-    });
-    if (!this.selectedDeviceAddress && this.deviceList.length > 0) {
-      this.selectedDeviceAddress = this.deviceList[0].address;
-    }
-  },
-
-  selectedDeviceAddressChanged: function() {
     var self = this;
-    if (!self.selectedDeviceAddress) {
+    self.bluetoothState = 'bluetooth-searching';
+    navigator.bluetooth.requestDevice([{
+      services: [
+        Sensortag.uuid.temperature,
+        Sensortag.uuid.accelerometer,
+        Sensortag.uuid.humidity,
+      ],
+    }], {
+      optionalServices: [],
+    }).then(function(device) {
+      self.connectingDevice = device;
+      var connected = Promise.resolve();
+      if (!device.connected) {
+        connected = device.connect();
+      }
+      connected.then(function() {
+        self.selectedDevice = device;
+      }).catch(function(error) {
+        if (error) {
+          self.error(error);
+        } else {
+          self.error('Could not connect to "' + device.name +
+                     '" (' + device.address + ')')
+        }
+      }).then(function() {
+        self.connectingDevice = null;
+      });
+    }, function(error) {
+      self.bluetoothState =
+          self.selectedDevice ? 'bluetooth-connected' : 'bluetooth';
+      if (error) {
+        console.error(error);
+      }
+    });
+  },
+
+  disconnect: function() {
+    var self = this;
+    if (self.selectedDevice) {
+      self.selectedDevice.disconnect().catch(window.onerror);
       self.selectedDevice = null;
-    } else {
-      var origSelectedAddress = self.selectedDeviceAddress;
-      navigator.bluetooth.getDevice(origSelectedAddress
-        ).then(function(device) {
-          if (self.selectedDeviceAddress === origSelectedAddress) {
-            self.selectedDevice = device;
-          }
-        }).catch(window.onerror);
     }
   },
 
@@ -127,17 +93,22 @@ Polymer('sensortag-main', {
     self.temperature.clearService();
     self.accelerometer.clearService();
     self.humidity.clearService();
-    Promise.all([
-      self.selectedDevice.getService(Sensortag.uuid.temperature).then(function(service) {
-        self.temperature.setService(service);
-      }),
-      self.selectedDevice.getService(Sensortag.uuid.accelerometer).then(function(service) {
-        self.accelerometer.setService(service);
-      }),
-      self.selectedDevice.getService(Sensortag.uuid.humidity).then(function(service) {
-        self.humidity.setService(service);
-      }),
-    ]).catch(window.onerror);
+    if (self.selectedDevice) {
+      self.bluetoothState = 'bluetooth-connected';
+      Promise.all([
+        self.selectedDevice.getService(Sensortag.uuid.temperature).then(function(service) {
+          self.temperature.setService(service);
+        }),
+        self.selectedDevice.getService(Sensortag.uuid.accelerometer).then(function(service) {
+          self.accelerometer.setService(service);
+        }),
+        self.selectedDevice.getService(Sensortag.uuid.humidity).then(function(service) {
+          self.humidity.setService(service);
+        }),
+      ]).catch(window.onerror);
+    } else {
+      self.bluetoothState = 'bluetooth';
+    }
   },
 });
 
